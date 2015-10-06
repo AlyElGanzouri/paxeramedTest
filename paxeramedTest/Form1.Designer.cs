c8edf1a1f89a00afe30cd82f9e007445090ffd7e
Aly ElGanzouri <contact@alyelganzouri.com>
2015-10-06 13:53:25 +0200

    First push

---
+++ paxeramedTest/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace paxeramedTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonGreyScale = new System.Windows.Forms.Button();
+            this.buttonInverse = new System.Windows.Forms.Button();
+            this.zoomBar = new System.Windows.Forms.TrackBar();
+            this.widthBox = new System.Windows.Forms.TextBox();
+            this.heightBox = new System.Windows.Forms.TextBox();
+            this.dateBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.lengthBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.zoomBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(787, 101);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(709, 587);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // buttonGreyScale
+            // 
+            this.buttonGreyScale.Enabled = false;
+            this.buttonGreyScale.Location = new System.Drawing.Point(787, 157);
+            this.buttonGreyScale.Name = "buttonGreyScale";
+            this.buttonGreyScale.Size = new System.Drawing.Size(75, 23);
+            this.buttonGreyScale.TabIndex = 0;
+            this.buttonGreyScale.Text = "GreyScale";
+            this.buttonGreyScale.UseVisualStyleBackColor = true;
+            this.buttonGreyScale.Click += new System.EventHandler(this.buttonGreyScale_Clicked);
+            // 
+            // buttonInverse
+            // 
+            this.buttonInverse.Enabled = false;
+            this.buttonInverse.Location = new System.Drawing.Point(787, 211);
+            this.buttonInverse.Name = "buttonInverse";
+            this.buttonInverse.Size = new System.Drawing.Size(75, 23);
+            this.buttonInverse.TabIndex = 3;
+            this.buttonInverse.Text = "Inverse";
+            this.buttonInverse.UseVisualStyleBackColor = true;
+            this.buttonInverse.Click += new System.EventHandler(this.buttonInverse_Click);
+            // 
+            // zoomBar
+            // 
+            this.zoomBar.Enabled = false;
+            this.zoomBar.LargeChange = 1;
+            this.zoomBar.Location = new System.Drawing.Point(727, 75);
+            this.zoomBar.Name = "zoomBar";
+            this.zoomBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.zoomBar.Size = new System.Drawing.Size(45, 262);
+            this.zoomBar.TabIndex = 4;
+            this.zoomBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.zoomBar.Value = 5;
+            this.zoomBar.Scroll += new System.EventHandler(this.zoomBar_Scroll);
+            // 
+            // widthBox
+            // 
+            this.widthBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.widthBox.Location = new System.Drawing.Point(727, 458);
+            this.widthBox.Name = "widthBox";
+            this.widthBox.ReadOnly = true;
+            this.widthBox.Size = new System.Drawing.Size(145, 20);
+            this.widthBox.TabIndex = 5;
+            // 
+            // heightBox
+            // 
+            this.heightBox.Location = new System.Drawing.Point(727, 484);
+            this.heightBox.Name = "heightBox";
+            this.heightBox.ReadOnly = true;
+            this.heightBox.Size = new System.Drawing.Size(145, 20);
+            this.heightBox.TabIndex = 6;
+            // 
+            // dateBox
+            // 
+            this.dateBox.Location = new System.Drawing.Point(727, 510);
+            this.dateBox.Name = "dateBox";
+            this.dateBox.ReadOnly = true;
+            this.dateBox.Size = new System.Drawing.Size(145, 20);
+            this.dateBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(727, 433);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Picture Information:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(724, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Zoom Slider";
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.Enabled = false;
+            this.buttonSaveFile.Location = new System.Drawing.Point(787, 258);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveFile.TabIndex = 10;
+            this.buttonSaveFile.Text = "Save File";
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // lengthBox
+            // 
+            this.lengthBox.Location = new System.Drawing.Point(727, 562);
+            this.lengthBox.Name = "lengthBox";
+            this.lengthBox.ReadOnly = true;
+            this.lengthBox.Size = new System.Drawing.Size(145, 20);
+            this.lengthBox.TabIndex = 11;
+            this.lengthBox.Text = "Line Length:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 611);
+            this.Controls.Add(this.lengthBox);
+            this.Controls.Add(this.buttonSaveFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateBox);
+            this.Controls.Add(this.heightBox);
+            this.Controls.Add(this.widthBox);
+            this.Controls.Add(this.zoomBar);
+            this.Controls.Add(this.buttonInverse);
+            this.Controls.Add(this.buttonGreyScale);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonOpenFile);
+            this.MaximumSize = new System.Drawing.Size(900, 650);
+            this.MinimumSize = new System.Drawing.Size(900, 650);
+            this.Name = "Form1";
+            this.Text = "Paxeramed ";
+            ((System.ComponentModel.ISupportInitialize)(this.zoomBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonGreyScale;
+        private System.Windows.Forms.Button buttonInverse;
+        private System.Windows.Forms.TrackBar zoomBar;
+        private System.Windows.Forms.TextBox widthBox;
+        private System.Windows.Forms.TextBox heightBox;
+        private System.Windows.Forms.TextBox dateBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonSaveFile;
+        private System.Windows.Forms.TextBox lengthBox;
+    }
+}
+
---
+++ paxeramedTest/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace paxeramedTest
+{
+    public partial class Form1 : Form
+    {
+        
+        private Bitmap sourceImg; //Bitmap for loaded image
+        
+        float ratio, xPos, yPos, xStartPos, yStartPos, xCurrent, yCurrent, initialRatio;
+
+        //variables for drawing the ling
+        float startX, endX, startY, endY;
+        //State checks
+        bool boolDrawLine = false;
+        bool boolFirstImage  = false;
+        bool boolMoveImage = false;
+        bool boolGrayScale = false;
+        bool boolInverse = false;
+        bool boolSaveFile = false;
+
+        //Color matrices used to convert image to matching format
+        ImageAttributes CSAttributes = new ImageAttributes();
+        ColorMatrix greyScaleMatrix = new ColorMatrix ( new float[][] {
+                 new float[] {.3f, .3f, .3f, 0, 0},
+                 new float[] {.59f, .59f, .59f, 0, 0},
+                 new float[] {.11f, .11f, .11f, 0, 0},
+                 new float[] {0, 0, 0, 1, 0},
+                 new float[] {0, 0, 0, 0, 1} });
+        ColorMatrix inverseMatrix = new ColorMatrix ( new float[][] {
+                 new float[] {-1, 0, 0, 0, 0},
+                 new float[] {0, -1, 0, 0, 0},
+                 new float[] {0, 0, -1, 0, 0},
+                 new float[] {0, 0, 0, 1, 0},
+                 new float[] {1, 1, 1, 0, 1} });
+        ColorMatrix inverseGreyScaleMatrix = new ColorMatrix ( new float[][] {
+                 new float[] {-.3f, -.3f, -.3f, 0, 0},
+                 new float[] {-.59f, -.59f, -.59f, 0, 0},
+                 new float[] {-.11f, -.11f, -.11f, 0, 0},
+                 new float[] {0, 0, 0, 1, 0},
+                 new float[] {1, 1, 1, 0, 1} });
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog()){
+                openFileDialog1.Title = "Open Image";
+                //Only allow bmp, JPEG, JPG, and png files
+                openFileDialog1.Filter = "Image Files (*.bmp, *.jpeg, *.jpg, *.png)|*.bmp; *.jpeg; *.jpg; *.png";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK){
+                    //Reset Values
+                    xPos = yPos = xCurrent = yCurrent = 0.0F;
+                    zoomBar.Value = 5;
+
+                    sourceImg = new Bitmap(openFileDialog1.FileName);
+                    //Write File information to text boxes 
+                    widthBox.Text = "Width: " + sourceImg.Width + " px";
+                    heightBox.Text = "Height: " + sourceImg.Height + " px";
+                    DateTime fileCreatedDate = File.GetCreationTime(openFileDialog1.FileName);
+                    dateBox.Text = "Created: " + fileCreatedDate;
+
+                    //Calculate the ratio to scale image to
+                    if (sourceImg.Width > sourceImg.Height){
+                        ratio = (float)panel1.Width / (float)sourceImg.Width;
+                    }
+                    else{
+                        ratio = (float)panel1.Height / (float)sourceImg.Height;
+                    }
+                    initialRatio = ratio;
+                    //X and Y shift to Center the image
+                    if (panel1.Width > (sourceImg.Width * ratio)){
+                        xPos = (((float)panel1.Width/ratio) - (float)sourceImg.Width) / 2;
+                    }
+                    if (panel1.Height > (sourceImg.Height * ratio)){
+                        yPos = (((float)panel1.Height / ratio) - (float)sourceImg.Height) / 2;
+                    }
+
+                    //Enable the buttons after first image
+                    if (boolFirstImage == false){
+                        this.panel1.Paint += new PaintEventHandler(panel1_Paint);
+                        boolFirstImage = true;
+                        buttonGreyScale.Enabled = true;
+                        buttonInverse.Enabled = true;
+                        zoomBar.Enabled = true;
+                        buttonSaveFile.Enabled = true;
+                    }
+                    this.panel1.Invalidate();
+                }
+            }
+        }
+
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e){
+           //Save starting positions for when moving the image or drawing line
+
+            if (e.Button == MouseButtons.Left){
+                xStartPos = e.X;
+                yStartPos = e.Y;
+                boolMoveImage = true;
+            } 
+            if (e.Button == MouseButtons.Right){
+                startX = e.X *(1/ratio) - xPos;
+                startY = e.Y *(1/ratio) - yPos;
+                boolDrawLine = true;
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e){
+            //Save ending positions for when moving the image or drawing line
+
+            if (e.Button == MouseButtons.Left && boolMoveImage == true  ){
+                panel1.Cursor = Cursors.Hand;
+                xPos = xCurrent + ((e.X - xStartPos) * (1 / ratio));
+                yPos = yCurrent + ((e.Y - yStartPos) * (1 / ratio));
+                panel1.Invalidate();
+            }
+
+            if (e.Button == MouseButtons.Right && boolDrawLine == true){
+                panel1.Cursor = Cursors.Cross;
+                endX = e.X * ( 1/ratio) - xPos;
+                endY = e.Y * ( 1/ratio) - yPos;
+                //Display line length
+                int length = Math.Abs(((int)endX - (int)startX)^2) + Math.Abs(((((int)endY - (int)startY)^2)) ^ (1 / 2));
+                lengthBox.Text = "Line Length: " + length.ToString() + " px";
+                panel1.Invalidate();
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e){
+            //Exit state
+
+            if (e.Button == MouseButtons.Left){
+                panel1.Cursor = Cursors.Default;
+                boolMoveImage = false;
+                xCurrent = xPos;
+                yCurrent = yPos;
+            }
+
+            if (e.Button == MouseButtons.Right){
+                panel1.Cursor = Cursors.Default;
+                boolDrawLine = false;
+            }
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Graphics graphicsObj = e.Graphics;
+            graphicsObj.ScaleTransform(ratio , ratio);
+            graphicsObj.TranslateTransform(xPos, yPos);
+
+            //Draw image depnding on color matrix
+            if (boolGrayScale == true && boolInverse == true){
+                CSAttributes.SetColorMatrix(inverseGreyScaleMatrix);
+                graphicsObj.DrawImage(sourceImg, new Rectangle(0, 0, sourceImg.Width, sourceImg.Height),
+                                0, 0, sourceImg.Width, sourceImg.Height, GraphicsUnit.Pixel, CSAttributes);
+            }
+            else if (boolGrayScale == true ){
+                CSAttributes.SetColorMatrix(greyScaleMatrix);
+                graphicsObj.DrawImage(sourceImg, new Rectangle(0, 0, sourceImg.Width, sourceImg.Height),
+                                 0, 0, sourceImg.Width, sourceImg.Height, GraphicsUnit.Pixel, CSAttributes);
+            }
+            else if (boolInverse == true){
+                CSAttributes.SetColorMatrix(inverseMatrix);
+                graphicsObj.DrawImage(sourceImg, new Rectangle(0, 0, sourceImg.Width, sourceImg.Height),
+                                 0, 0, sourceImg.Width, sourceImg.Height, GraphicsUnit.Pixel, CSAttributes);
+            }
+            else{
+                graphicsObj.DrawImage(sourceImg, 0, 0, sourceImg.Width, sourceImg.Height);
+            }
+
+            //Draw line
+            Pen blackPen = new Pen(Color.Black, 5*(1/ratio));            
+            Point point1 = new Point((int)startX, (int)startY);
+            Point point2 = new Point((int)endX, (int)endY);
+            graphicsObj.DrawLine(blackPen, point1, point2);
+
+            //Save file if required
+            if (boolSaveFile == true){
+                boolSaveFile = false;
+                using (SaveFileDialog saveFileDialog1 = new SaveFileDialog()){     
+                    saveFileDialog1.Title = "Save Image";
+                    saveFileDialog1.Filter = "Image Files (*.bmp, *.jpeg, *.jpg, *.png)|*.bmp; *.jpeg; *.jpg; *.png";
+
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK){
+                        using (Bitmap savedBitmap = new Bitmap(panel1.ClientSize.Width, panel1.ClientSize.Height)){
+                            panel1.DrawToBitmap(savedBitmap, panel1.ClientRectangle);
+                            savedBitmap.Save(saveFileDialog1.FileName);   
+                        }
+                    }
+                }  
+            }
+        }
+
+        //Enter or exit grey scale state
+        private void buttonGreyScale_Clicked(object sender, EventArgs e){
+            boolGrayScale = !boolGrayScale;
+            panel1.Invalidate();
+        }
+
+        //Enter or exit inverse state
+        private void buttonInverse_Click(object sender, EventArgs e){
+            boolInverse = !boolInverse;
+            panel1.Invalidate();
+        }
+
+        //Zoom image: set ratio and calculate new center
+        //Had to use a bar, did not have middle mouse button or scroll wheel
+        //but technically its the same, only thing that would change is the event handler 
+        private void zoomBar_Scroll(object sender, EventArgs e){
+            if (zoomBar.Value == 0){
+                ratio = 0.1F * initialRatio ;
+            }
+            else{
+                ratio = initialRatio * zoomBar.Value * 2.0F   / 10.0F;
+            }
+
+            xPos = ((panel1.Width / ratio) - sourceImg.Width ) / 2;
+            yPos = ((panel1.Height / ratio) - sourceImg.Height) / 2;
+            panel1.Invalidate();
+        }
+
+        //Enter save file state 
+        private void buttonSaveFile_Click(object sender, EventArgs e){
+            boolSaveFile = true;
+            panel1.Invalidate();
+        }
+    }
+}
